af0961e0cbe23793554fa670d83929ec2abcd92e
QberToff <74160641+QberToff@users.noreply.github.com>
2022-06-21 02:49:30 +0300

    edit alien test

--- test_param_aliens.py
+++ test_param_aliens.py
@@ -6,33 +6,35 @@ from  selenium.webdriver.common.by import By
 import time
 import math
 
-@pytest.fixture(scope = "function")
+message = ''
+
+@pytest.fixture(scope="function")
 def browser():
     browser = webdriver.Chrome()
     yield browser
+    global message
+    print(message)
     browser.quit()
 
-
-
 @pytest.mark.parametrize('pages', ['236895','236896','236897','236898','236899','236903','236904','236905'])
-class Test_aliens:
-    message = ''
+def test_for_answer_form(browser, pages):
+    global message
+    link = f"https://stepik.org/lesson/{pages}/step/1"
+    print (f"open link {pages}")
+    browser.get(link)
+    print('trying to find textarea')
+    browser.implicitly_wait(5)
+    textarea = browser.find_element(By.XPATH, "//textarea")
+    print('textarea was found')
+    textarea.send_keys(str(math.log(int(time.time() + 1.4))))
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+    )
+    button.click()
+    hint = WebDriverWait(browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@class='smart-hints__hint']")))
+    try:
+        assert hint.text == "Correct!"
+    except AssertionError:
+        message += hint.text
 
 
-    def test_for_answer_form(self, browser, pages):
-        link = f"https://stepik.org/lesson/{pages}/step/1"
-        print (f"open link {pages}")
-        browser.get(link)
-        print('trying to find textarea')
-        browser.implicitly_wait(5)
-        textarea = browser.find_element(By.XPATH, "//textarea")
-        print('textarea was found')
-        textarea.send_keys(str(math.log(int(time.time() + 1.4))))
-        button = WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
-        )
-        button.click()
-        hint = WebDriverWait(browser,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@class='smart-hints__hint']")))
-        assert hint.text != "Correct!"
-        self.message += hint.text
-        print(self.message)
